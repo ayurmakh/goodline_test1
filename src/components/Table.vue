299ccdef43529626bf94d26e5b057e568cf395d8
Ayur <ayurmakhutoff@gmail.com>
2019-06-19 21:29:09 +0800

    init

---
+++ src/components/Table.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <table>
+      <th v-for="header in headers" :key="header">{{ header }}</th>
+      <tr v-for="(row, i) in table" :key="i">
+        <td v-for="(val, j) in row" :key="j">{{ val }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import json from '../data.json'
+
+export default {
+  name: 'Table',
+  data() {
+    return {
+      headers: ['name', 'id', 'phone','age'],
+      table: json
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+table {
+  font-size: 20px;
+  width: 700px
+}
+td {
+  text-align: center;
+}
+</style>
